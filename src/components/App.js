e87714679292ab608e0c573002d80e9a9f57b387
Luiz <luiz.oamorim@gmail.com>
2021-05-05 22:02:51 -0300

    feat: starting frontend customizition

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './Navbar'
+import './App.css'
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable()
+    } else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("Non-Ethereum browser detected. You should consider trying Metamask!");
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '0x0'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar account={this.state.account} />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
+              <div className="content mr-auto ml-auto">
+                <a
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                </a>
+
+                <h1>Hello, World!</h1>
+
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
