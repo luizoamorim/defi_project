74a7e45da4d02542c930e8cbb2c53c6deb696483
Luiz <luiz.oamorim@gmail.com>
2021-05-05 22:06:12 -0300

    feat: creating scripts

---
+++ scripts/issue-token.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+const TokenFarm = artifacts.require("TokenFarm");
+
+module.exports = async function(callback) {
+  let tokenFarm = await TokenFarm.deployed();
+  await tokenFarm.issueTokens();
+  
+  // Code goes here...
+  console.log("Tokens issued!")
+
+  callback();
+};
